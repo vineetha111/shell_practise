9bc8fe8dc5c2a5fa1651ec6dbf34b839b304bbb7
vineetha <seelamvineetha@gmail.com>
2025-11-21 10:03:29 +1100

    new sh file commited

---
+++ conversations.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+echo "Trump:: Hey Musk, How are you?"
+echo "Musk:: Hello Trump, I am fine. How are you doing"
+echo "Trump: I am fine too. What's up?"
+echo "Musk:: Nohting, just going to Mars now, will you come?"
+echo "Trump:: Sorry, you carry on! I will come once you come back"
